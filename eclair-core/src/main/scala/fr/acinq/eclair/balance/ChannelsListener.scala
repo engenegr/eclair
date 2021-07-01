0f933fe9646c247ff68ca495ff6f69854e7b2e3d
pm47 <pm.padiou@gmail.com>
2021-07-01 17:23:52 +0200

    improve the event stream synchronization trick

--- eclair-core/src/main/scala/fr/acinq/eclair/balance/ChannelsListener.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/balance/ChannelsListener.scala
@@ -7,11 +7,12 @@ import akka.actor.typed.eventstream.EventStream
 import akka.actor.typed.scaladsl.adapter.ClassicActorRefOps
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import fr.acinq.bitcoin.ByteVector32
-import fr.acinq.eclair.balance.ChannelsListener.{ChannelData, ChannelDied, Command, GetChannels, GetChannelsResponse}
+import fr.acinq.eclair.balance.ChannelsListener._
 import fr.acinq.eclair.channel.Helpers.Closing
 import fr.acinq.eclair.channel.{ChannelPersisted, ChannelRestored, HasCommitments}
 
 import scala.concurrent.Promise
+import scala.concurrent.duration.DurationInt
 
 object ChannelsListener {
 
@@ -20,6 +21,8 @@ object ChannelsListener {
   private final case class ChannelData(channelId: ByteVector32, channel: akka.actor.ActorRef, data: HasCommitments) extends Command
   private final case class ChannelDied(channelId: ByteVector32) extends Command
   final case class GetChannels(replyTo: typed.ActorRef[GetChannelsResponse]) extends Command
+  final case object SendDummyEvent extends Command
+  final case object DummyEvent extends Command
   // @formatter:on
 
   case class GetChannelsResponse(channels: Map[ByteVector32, HasCommitments])
@@ -28,13 +31,22 @@ object ChannelsListener {
     Behaviors.setup { context =>
       context.system.eventStream ! EventStream.Subscribe(context.messageAdapter[ChannelRestored](e => ChannelData(e.channelId, e.channel, e.data)))
       context.system.eventStream ! EventStream.Subscribe(context.messageAdapter[ChannelPersisted](e => ChannelData(e.channelId, e.channel, e.data)))
-      // since subscription is asynchronous, we send a fake event so we know when we are subscribed
-      context.system.eventStream ! EventStream.Publish(ChannelRestored(null, ByteVector32.Zeroes, null, null, null))
-      Behaviors.receiveMessagePartial {
-        case ChannelData(channelId, _, _) if channelId == ByteVector32.Zeroes =>
-          context.log.info("channels listener is ready")
-          ready.success(Done)
-          new ChannelsListener(context).running(Map.empty)
+      context.system.eventStream ! EventStream.Subscribe(context.self.narrow[DummyEvent.type])
+      Behaviors.withTimers { timers =>
+        // since subscription is asynchronous, we send a fake event so we know when we are subscribed
+        val dummyTimer = "dummy-event"
+        timers.startTimerAtFixedRate(dummyTimer, SendDummyEvent, 100 milliseconds)
+        Behaviors.receiveMessagePartial {
+          case SendDummyEvent =>
+            context.system.eventStream ! EventStream.Publish(DummyEvent)
+            Behaviors.same
+          case DummyEvent =>
+            context.log.info("channels listener is ready")
+            timers.cancel(dummyTimer)
+            context.system.eventStream ! EventStream.Unsubscribe(context.self.narrow[DummyEvent.type])
+            ready.success(Done)
+            new ChannelsListener(context).running(Map.empty)
+        }
       }
     }
 }
@@ -64,5 +76,7 @@ private class ChannelsListener(context: ActorContext[Command]) {
       case GetChannels(replyTo) =>
         replyTo ! GetChannelsResponse(channels)
         Behaviors.same
+      case SendDummyEvent => Behaviors.same
+      case DummyEvent => Behaviors.same
     }
 }
